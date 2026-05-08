0634f54a25dd43fa77f30a26c846cc6c3e3d624c
Chen <fnel2003@naver.com>
2026-05-08 17:49:43 +0900

    feat: new upload

--- app/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Dashboard() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Dashboard</h1>
-      <a href="/test1" className="text-blue-500 hover:underline p-4">Go to Test1</a>
-    </div>
-  );
-}
---
+++ app/test/page.tsx
@@ -0,0 +1,7 @@
+export default function domin() {
+    return(
+        <div>
+            <h1 className="text-blue-500 ">idfdfasdfasuk</h1>
+        </div>
+    )
+}
