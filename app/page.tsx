a6f24ba2ea0c0c77aa074562eaec668b87a02256
jinwoo0501 <168405013+jinwoo0501@users.noreply.github.com>
2026-05-08 17:40:10 +0900

    JIN

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!dqdwqdwqdqwdqwdwqqwdwqdqwdqwdqwdwqdqwdwdqwdwqdwqddwq</h1>
     </div>
   );
 }
